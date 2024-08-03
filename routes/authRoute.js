e859e3fc67d8923fdbab4ed4ddcf95dde5e0b19f
codersunny812 <sengersunny448@gmail.com>
2024-08-03 17:42:36 +0530

    code updated

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
 
+router.get('/register',(req,res)=>{
+    res.render('register');
+});
 router.post('/register', register);
+router.get('/login',(req,res)=>{
+    res.render('login');
+});
 router.post('/login', login);
 
 module.exports = router;
